ff6924d855586cb1b541a6bc3ec95aa318c579ac
Vesna Tanko <tankovesna@hotmail.com>
2019-12-02 19:35:01 +0100

    TestTopicModeling: Test saved selection

--- orangecontrib/text/widgets/tests/test_owtopicmodeling.py
+++ orangecontrib/text/widgets/tests/test_owtopicmodeling.py
@@ -1,42 +1,50 @@
 import unittest
 
-from AnyQt.QtCore import QItemSelection, QItemSelectionRange, \
-    QItemSelectionModel, QModelIndex
+import numpy as np
+from AnyQt.QtCore import QItemSelectionModel
+
 from Orange.widgets.tests.base import WidgetTest
-from PyQt5 import Qt
-from PyQt5.QtTest import QTest
 from orangecontrib.text.corpus import Corpus
 from orangecontrib.text.widgets.owtopicmodeling import OWTopicModeling
 
 
 class TestTopicModeling(WidgetTest):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.corpus = Corpus.from_file('deerwester')
+
     def setUp(self):
         self.widget = self.create_widget(OWTopicModeling)
-        self.corpus = Corpus.from_file('deerwester')
 
     def test_data(self):
-        self.send_signal(self.widget.Inputs.corpus, self.corpus)
-        # otherwise fails in learning task as if it was sent None
-        self.wait_until_finished()
-        self.send_signal(self.widget.Inputs.corpus, None)
-        self.wait_until_finished()
-        
-    def test_saved_selection(self):
-        self.send_signal(self.widget.Inputs.corpus, self.corpus)
-        self.wait_until_finished()
-        id = self.widget.topic_desc.model().index(2, 0)
-        self.widget.topic_desc.selectionModel().select(id,
-                                                       QItemSelectionModel.Rows |
-                                                       QItemSelectionModel.Select)
-        ids_1 = self.get_output(self.widget.Outputs.selected_topic).ids
-        state = self.widget.settingsHandler.pack_data(self.widget)
-        w = self.create_widget(
-            OWTopicModeling, stored_settings=state
-        )
-        self.send_signal(w.Inputs.corpus, self.corpus, widget=w)
-        ids_2 = self.get_output(w.Outputs.selected_topic, widget=w).ids
-        self.assertSequenceEqual(list(ids_1), list(ids_2))
+        def until():
+            return bool(self.get_output(self.widget.Outputs.selected_topic))
 
+        self.send_signal(self.widget.Inputs.corpus, self.corpus)
+        self.process_events(until)
+
+        self.send_signal(self.widget.Inputs.corpus, None)
+        output = self.get_output(self.widget.Outputs.selected_topic)
+        self.assertIsNone(output)
+
+    def test_saved_selection(self):
+        def until(widget=self.widget):
+            return bool(self.get_output(widget.Outputs.selected_topic))
+
+        self.send_signal(self.widget.Inputs.corpus, self.corpus)
+        self.process_events(until)
+        idx = self.widget.topic_desc.model().index(2, 0)
+        self.widget.topic_desc.selectionModel().select(
+            idx, QItemSelectionModel.Rows | QItemSelectionModel.ClearAndSelect)
+        output1 = self.get_output(self.widget.Outputs.selected_topic)
+        state = self.widget.settingsHandler.pack_data(self.widget)
+
+        w = self.create_widget(OWTopicModeling, stored_settings=state)
+        self.send_signal(w.Inputs.corpus, self.corpus, widget=w)
+        self.process_events(lambda: until(w))
+        output2 = self.get_output(w.Outputs.selected_topic, widget=w)
+        np.testing.assert_allclose(output1, output2)
 
 
 if __name__ == "__main__":
